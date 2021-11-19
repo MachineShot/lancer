012c89b4e08f07a8c1a8d2d97abfbf7e200b3356
machineshot <16322422+MachineShot@users.noreply.github.com>
2021-11-19 02:55:57 +0200

    Authentication and roles

--- src/main/java/com/example/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/example/ReactAndSpringDataRestApplication.java
@@ -1,15 +1,10 @@
 package com.example;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactAndSpringDataRestApplication implements CommandLineRunner {
+public class ReactAndSpringDataRestApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
     }
 }
