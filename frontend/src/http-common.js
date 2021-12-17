dc7c989dfb8fb2e0d150f2287802cc66817ff8c5
machineshot <16322422+MachineShot@users.noreply.github.com>
2021-12-17 02:22:39 +0200

    URL change

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from './services/auth-header';
 
 export default axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://lancer-stpp.herokuapp.com:8080",
     headers: {
         "Content-type": "application/json",
         "Authorization": authHeader()
